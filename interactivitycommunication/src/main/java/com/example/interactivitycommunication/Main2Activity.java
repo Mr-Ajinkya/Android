1d14ca996c66401f027baf29bfd1a3b1572aa659
Mr-Ajinkya <Virkud.ajinkya2248@gmail.com>
2017-02-07 12:32:53 +0530

    inter activity communication,send data to another activity

--- interactivitycommunication/src/main/java/com/example/interactivitycommunication/Main2Activity.java
+++ interactivitycommunication/src/main/java/com/example/interactivitycommunication/Main2Activity.java
@@ -3,6 +3,7 @@ package com.example.interactivitycommunication;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -10,6 +11,13 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+            Intent resonseIntent = getIntent();
+        Bundle bundle = resonseIntent.getExtras();
+        if(bundle != null){
+            String email = bundle.getString(MainActivity.KEY_EMAIL);
+                       ((EditText) findViewById(R.id.edtEmail)).setText(email);
+        }
 
     }
 }
--- interactivitycommunication/src/main/java/com/example/interactivitycommunication/MainActivity.java
+++ interactivitycommunication/src/main/java/com/example/interactivitycommunication/MainActivity.java
@@ -4,24 +4,37 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 import static com.example.interactivitycommunication.R.id.edtEmail;
 
 public class MainActivity extends AppCompatActivity {
+
+    static final String KEY_EMAIL = "email";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         findViewById(R.id.btnNext).setOnClickListener(this::goNext);
+
     }
 
     private void goNext(View view) {
 
-        Intent  intent = new Intent(this, Main2Activity.class);
-        //Intent.putExtras("edtEmail",edtEmail);
+        Intent  intent = new Intent(this, Main2Activity.class);//Start new activity
+
+        Bundle bundle = new Bundle();
+        bundle.putString(KEY_EMAIL,getEmail());
+        intent.putExtras(bundle);
 
         startActivity(intent);
+    }
+
+    private String getEmail() {
+        return ((EditText)findViewById(R.id.edtEmail)).getText().toString();
+
     }
 
 
