f8593be4bf55e93f3388e098b06579a3ab1a02bc
Mr-Ajinkya <Virkud.ajinkya2248@gmail.com>
2017-02-16 12:58:43 +0530

    List View with Array Adapter and add content to list

--- listview/src/main/java/com/example/listview/MainActivity.java
+++ listview/src/main/java/com/example/listview/MainActivity.java
@@ -2,18 +2,21 @@ package com.example.listview;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ArrayList<String> dataSet;
+    private ArrayAdapter<String> adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ArrayList<String> dataSet = new ArrayList<>();
+        dataSet = new ArrayList<>();
         dataSet.add("India");
         dataSet.add("Russia");
         dataSet.add("Nepal");
@@ -22,9 +25,16 @@ public class MainActivity extends AppCompatActivity {
         dataSet.add("Bhutan");
         dataSet.add("America");
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,dataSet);
+        adapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,dataSet);
 
         ListView listNation = (ListView) findViewById(R.id.listNation);
         listNation.setAdapter(adapter);
+        findViewById(R.id.btnAdd).setOnClickListener(this::click);
+    }
+
+    private void click(View view) {
+        String typeNat = ((EditText) findViewById(R.id.edtText)).getText().toString();
+        if(typeNat.length() > 0)
+        dataSet.add(typeNat);
     }
 }
