c679ca38df4387e13768485c7437f84b660b5b69
Mr-Ajinkya <Virkud.ajinkya2248@gmail.com>
2017-03-16 14:31:09 +0530

    Program-Alert Dialog

---
+++ dialogs/src/main/java/com/example/dialogs/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.dialogs;
+
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btnAlert).setOnClickListener(this::clicked);
+    }
+
+    private void clicked(View view) {
+
+        if (view.getId() == R.id.btnAlert) showAlert();
+    }
+
+    private void showAlert() {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setIcon(R.mipmap.ic_launcher)
+                .setMessage(R.string.message)
+                .setTitle(R.string.title);
+        AlertDialog dialog = builder.create();
+        dialog.show();
+
+    }
+}
