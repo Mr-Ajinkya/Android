e36bdf4fafb934136b306bbd46a1253122a29d02
Mr-Ajinkya <Virkud.ajinkya2248@gmail.com>
2017-04-05 13:01:36 +0530

    Program- Android Material Design(Basic layout,Option Menu,Context Menu)

---
+++ materialdesign/src/main/java/codekulmanagement/codekul/com/materialdesign/MainActivity.java
@@ -0,0 +1,80 @@
+package codekulmanagement.codekul.com.materialdesign;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show());
+
+        registerForContextMenu(findViewById(R.id.edtUserName));
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.actionSettings) {
+             Toast.makeText(this, "Settings", Toast.LENGTH_SHORT).show();
+        }else if (id == R.id.actionProfile){
+            Toast.makeText(this, "Profile", Toast.LENGTH_SHORT).show();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+
+        if (v.getId() == R.id.edtUserName){
+            menu.add(100,1,0,"Cut");
+            menu.add(100,2,1,"Copy");
+        }
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        if (item.getItemId() == 1){
+            Toast.makeText(this, "Cut", Toast.LENGTH_SHORT).show();
+        }else {
+            Toast.makeText(this, "Copy", Toast.LENGTH_SHORT).show();
+        }
+        return true;
+    }
+}
