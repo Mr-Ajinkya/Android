70b949ec59c8610896ee83503b773fff28952eee
Mr-Ajinkya <Virkud.ajinkya2248@gmail.com>
2017-04-17 13:03:20 +0530

    Program-ListView with Array Adapter

---
+++ listtestexmple/src/main/java/codekulmanagement/codekul/com/listtestexmple/MainActivity.java
@@ -0,0 +1,33 @@
+package codekulmanagement.codekul.com.listtestexmple;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    String[] names = {"Ajinkya","Rahul","Sandeep","Priya","Aniruddha"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final ListView listView = (ListView) findViewById(R.id.listView);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,names);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                TextView name = (TextView) view;
+                Toast.makeText(getApplicationContext(),name.getText()+" "+position,Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+}
