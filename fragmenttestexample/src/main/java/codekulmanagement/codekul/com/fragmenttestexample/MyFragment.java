cf595c872e1092be86a3d1026523b66485800c2b
Mr-Ajinkya <Virkud.ajinkya2248@gmail.com>
2017-04-18 17:26:23 +0530

    Program- Adding fragment in XML

---
+++ fragmenttestexample/src/main/java/codekulmanagement/codekul/com/fragmenttestexample/MyFragment.java
@@ -0,0 +1,21 @@
+package codekulmanagement.codekul.com.fragmenttestexample;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Ajinkya Virkud on 4/18/2017.
+ */
+
+public class MyFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.my_fragment,container,false);
+    }
+}
